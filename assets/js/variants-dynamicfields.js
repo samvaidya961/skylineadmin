6e247e2973a8e0483ab6388f96b93ad3924a5c98
samvaidya961 <67541221+samvaidya961@users.noreply.github.com>
2021-08-12 17:26:44 +0530

    Add files via upload

--- assets/js/variants-dynamicfields.js
+++ assets/js/variants-dynamicfields.js
@@ -1,9 +1,17 @@
 // dynamic fields
 $(document).ready(function () {
-    var maxField = 20; //Input fields increment limitation
+    var maxField = 100; //Input fields increment limitation
     var addButton = $('.add_button1'); //Add button selector
     var wrapperip1 = $('.field_wrapper1'); //Input field wrapper
-    var fieldHTML = '<div class="mb-4"><input type="text" class="form-control form-control-sm d-inline m-1 mb-2" name="field_name1[]" value=""/><a href="javascript:void(0);" class="remove_button mx-1"><span class="material-icons text-danger">remove_circle_outline</span></a><div class="d-flex variantmorevalues"><input type="text" class="form-control form-control-sm m-1" placeholder="MRP" name="field_name2[]"><input type="text" class="form-control form-control-sm m-1" placeholder="Discount" name="field_name3[]"><input type="text" class="form-control form-control-sm m-1" placeholder="GST" name="field_name4[]"><input type="text" class="form-control form-control-sm m-1" placeholder="Price" name="field_name5[]"><select class="form-select form-select-sm m-1" aria-label=".form-select-sm example" name="field_name6[]"><option selected>In Stock</option><option value="1">Out Of Stock</option></select></div></div>'; //New input field html
+
+
+    var fieldHTML = '<div class="mt-4 mb-2"><input type="text" class="form-control form-control-sm d-inline m-1 mb-2" name="field_name1[]" value="" placeholder="Variant Name"/><div class="d-flex variantmorevalues"><input type="text" class="form-control form-control-sm m-1" placeholder="MRP" name="field_name2[]"><input type="text" class="form-control form-control-sm m-1" placeholder="Discount" name="field_name3[]"><input type="text" class="form-control form-control-sm m-1" placeholder="GST" name="field_name4[]"><input type="text" class="form-control form-control-sm m-1" placeholder="Price" name="field_name5[]"><select class="form-select form-select-sm m-1" aria-label=".form-select-sm example" name="field_name6[]"><option selected>In Stock</option><option value="1">Out Of Stock</option></select></div></div>';
+
+    var fieldHTML2 = '<div class="prodvariants  mt-2 d-flex"> <input type="text" class="form-control form-control-sm w-50 d-inline m-1" placeholder="Property Name" name="field_namep1[]" value="Weight"><input type="text" class="form-control form-control-sm w-50 d-inline m-1" placeholder="Property Value" name="field_namep2[]"></div><div class="prodvariants  mt-2 d-flex"> <input type="text" class="form-control form-control-sm w-50 d-inline m-1" placeholder="Property Name" name="field_namep1[]" value="Country"><input type="text" class="form-control form-control-sm w-50 d-inline m-1" placeholder="Property Value" name="field_namep2[]"></div>';
+
+    var fieldHTML3='<a href="javascript:void(0);" class="remove_button mx-1"><span class="material-icons text-danger">remove_circle_outline</span></a>'
+    //New input field html
+
     var x = 1; //Initial field counter is 1
 
     //Once add button is clicked
@@ -11,9 +19,10 @@ $(document).ready(function () {
         //Check maximum number of input fields
         if (x < maxField) {
             x++; //Increment field counter
-            $(wrapperip1).append(fieldHTML); //Add field html
+            $(wrapperip1).append(fieldHTML, fieldHTML2); //Add field html
         }
     });
+
 
     //Once remove button is clicked
     $(wrapperip1).on('click', '.remove_button', function (e) {
@@ -21,5 +30,9 @@ $(document).ready(function () {
         $(this).parent('div').remove(); //Remove field html
         x--; //Decrement field counter
     });
+
+
+
 });
 
+// <a href="javascript:void(0);" class="remove_button mx-1"><span class="material-icons text-danger">remove_circle_outline</span></a>
